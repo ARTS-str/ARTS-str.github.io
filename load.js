999c61867bbeb694820113a94e16325d4e9196c5
ARTS-str <magninfacundo@gmail.com>
2025-02-20 01:07:10 -0300

    buffer stops when loading

--- load.js
+++ load.js
@@ -6,14 +6,12 @@ videos.forEach(video => {
   if (video.tagName === 'VIDEO') {
     video.addEventListener('canplay' ,() =>{
       loadPercent += 100/videos.length;
-      console.log(video, loadPercent);
     }, { once: true });
   }
 
   if (video.tagName === 'IMG') {
     video.addEventListener("load" ,() =>{
       loadPercent += 100/videos.length;
-      console.log(video, loadPercent);
     }, { once: true });
   }
 
@@ -21,7 +19,9 @@ videos.forEach(video => {
 
 
 function loadProgress() {
-
+  const containerBar = document.getElementById('degreeProgressBarContainer');
+  containerBar.style.animation = 'none';
+  containerBar.style.backgroundImage = 'none';
   const bar = document.getElementById('degreeProgressBar');
   bar.style.width = Math.trunc(loadPercent) + '%';
   countUpPercentaje();
